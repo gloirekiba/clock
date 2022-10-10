a5cc49b1d31afbb4dde0d9f0fd4197346ad11c92
gloirekiba <gloirekiba21@gmail.com>
2022-10-10 03:15:09 +0200

    ♻️ refactor code

--- script.js
+++ script.js
@@ -2,19 +2,24 @@ const container = document.createElement("div");
 container.classList.add("container");
 
 const span = document.createElement("span");
-span.classList.add("time");
 
 container.appendChild(span);
 document.body.appendChild(container);
 
 function getTime(date) {
-  return [date.getHours(), date.getMinutes(), date.getSeconds()];
+  return {
+    hour: date.getHours(),
+    minute: date.getMinutes(),
+    second: date.getSeconds(),
+  };
 }
 
 setInterval(() => {
-  const [hour, minute, second] = getTime(new Date());
+  const { hour, minute, second } = getTime(new Date());
 
-  span.innerText = `${hour.toString().padStart(2, "0")} : ${minute
+  const time = `${hour.toString().padStart(2, "0")} : ${minute
     .toString()
     .padStart(2, "0")} : ${second.toString().padStart(2, "0")}`;
+
+  span.innerText = time;
 }, 1000);
